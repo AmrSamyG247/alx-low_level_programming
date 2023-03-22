06a34c5bd168af23acc93cf782068f53a52e6666
AmrGreens247 <amr.samy@greens247.com>
2023-03-22 13:25:56 -0700

    gcc task

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -17,10 +17,8 @@ month = 2;
 day = 29;
 year = 2000;
 
-printf("Date: %02d/%02d/%04d\n", month, day , year);
-
+printf("Date: %02d/%02d/%04d/n", month, day, year);
 day = convert_day(month, day);
-
 print_remaining_days(month, day, year);
 
 return (0);
