24b2c0e258db1aec22d68881c6ef8c1435bce0e8
AmrGreens247 <amr.samy@greens247.com>
2023-04-05 14:45:56 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,29 @@
 #include "main.h"
 /**
+ * substring_check - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ * Return: 1 if matched, 0 if not
+ */
+
+int substring_check(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (substring_check(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s2 == '*')
+		return (substring_check(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (substring_check(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (substring_check(s1 + 1, after_wldcd, after_wldcd));
+}
+
+/**
  * wildcmp - compare if string with wildcard mattches
  * @s1: one string
  * @s2: one string
@@ -13,7 +37,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
-		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+		return (substring_check(s1, (s2 + 1), (s2 + 1)));
 	else
 		return (0);
 }
