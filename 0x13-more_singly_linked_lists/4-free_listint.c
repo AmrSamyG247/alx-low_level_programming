30afe0174d242714c0925d63d6155a409134447d
AmrGreens247 <amrsamy111@gmail.com>
2023-04-29 05:17:08 -0700

    Task 4 with gcc test files memory fix errors freeing mem check

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,13 +7,15 @@
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
-
+                
+	if (head == NULL)
+		return;
 
 	while (head != NULL)
 	{
 		tmp = head;
-		free(head);
 		head = tmp->next;
+		free(tmp);
 	}
 
 }
