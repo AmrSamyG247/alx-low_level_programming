36da088dfab671dd83e14537c9deeb63c341aa57
AmrGreens247 <amrsamy111@gmail.com>
2023-05-04 05:16:08 -0700

    Task 3 fix checker 4,5

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "2-get_bit.c"
 /**
  * set_bit - sets a bit
  * @n: integer to grab 
@@ -7,10 +6,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-	return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
 }
