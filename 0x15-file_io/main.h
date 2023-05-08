6fa13f8f9927441a25064ea63335729a331c4c22
AmrGreens247 <amrsamy111@gmail.com>
2023-05-08 12:45:37 -0700

    Task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,10 +10,10 @@
 #include <fcntl.h>
 #include <elf.h>
 
+#define BUFSIZE 1204
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 #endif
